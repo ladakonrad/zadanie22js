20dbe023b6fa36eb33cb957728665ecbfd6b9abe
Konrad <ladakonrad@gmail.com>
2018-10-11 20:47:38 +0200

    dodano zadanie22js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let liczbaPierwsza = 5;
+let liczbaDruga = 7;
+
+function sum(liczbaPierwsza, liczbaDruga) {
+    let suma = liczbaPierwsza + liczbaDruga;
+    console.log(suma);
+    return suma;
+
+};
+
+sum(5, 7);
